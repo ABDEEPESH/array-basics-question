f97846c082e26f3472b00dad028c5f09b008004c
ABDEEPESH <155074929+ABDEEPESH@users.noreply.github.com>
2025-07-07 12:53:54 +0530

    Create Cyclically Rotate an Array by One.java

---
+++ Cyclically Rotate an Array by One.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void rotateByOne(int[] arr) {
+        int last = arr[arr.length - 1];
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+    }
+}
